0afcf828fcf34928eff668e8a0b373fe73a64ba0
giorgia <g.sambrotta@gmail.com>
2020-09-17 15:26:42 +0200

    ignore build folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d5478b5970c0aad6735a";
+/******/ 	var hotCurrentHash = "ecdfb5971430a028d52a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
