a15a64ed554241425d9adf899a49de13a0e6dfa7
giorgia <designbygio@pm.me>
2020-11-11 18:17:43 +0000

    Add export config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 const { locales, defaultLocale } = require('./i18n')
 
 module.exports = {
-  i18n: { locales, defaultLocale }
+  i18n: {
+    locales,
+    defaultLocale
+  },
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/why-water': { page: 'why-water' },
+      '/404': { page: '404' }
+    }
+  }
 }
