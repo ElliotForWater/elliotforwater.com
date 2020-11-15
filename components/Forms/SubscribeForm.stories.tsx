4a06901acfea2d3a9489bd2d4c00367ad91a1d41
Conrad <36226614+ConradParker@users.noreply.github.com>
2020-11-15 09:45:43 +0100

    Add subscribe form component

--- components/Forms/SubscribeForm.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import SubscribeFormComponent from './SubscribeForm'
-
-export default { title: 'Components/Forms' }
-
-export const SubscribeForm = () => <SubscribeFormComponent />
