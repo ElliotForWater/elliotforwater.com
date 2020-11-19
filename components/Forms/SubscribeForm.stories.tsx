d5649b1eeb80dbe8b7f9db739d9f67544c03b80f
Conrad <36226614+ConradParker@users.noreply.github.com>
2020-11-19 13:15:40 +0100

    Tidy Code

---
+++ components/Forms/SubscribeForm.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import SubscribeFormComponent from './SubscribeForm'
+
+export default { title: 'Components/Forms' }
+
+export const SubscribeForm = () => <SubscribeFormComponent />
