444f9b0ed5b2c654e38992b6a0a982651e968928
Conrad <36226614+ConradParker@users.noreply.github.com>
2020-11-26 14:42:22 +0100

    Add unit tests

--- components/Toast/ToastList.test.tsx
+++ components/Toast/ToastList.test.tsx
@@ -29,7 +29,6 @@ describe('ToastList', () => {
       // Arrange
       const wrapper = mount(<ToastList {...{ toastList: toastList }} />)
       const firstToast = wrapper.findWhere((el) => el.is(Toast)).first()
-
       // Act
       firstToast.find('button').simulate('click')
 
