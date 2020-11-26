827b452ec780477318686702ff088f40cc456163
Conrad <36226614+ConradParker@users.noreply.github.com>
2020-11-26 14:42:22 +0100

    Add unit tests

--- components/Toast/Toasts.stories.tsx
+++ components/Toast/Toasts.stories.tsx
@@ -1,6 +1,39 @@
 import React from 'react'
 import ToastListComponent from './ToastList'
 
-export default { title: 'Components/Toasts' }
+export default {
+  title: 'Components/Toasts',
+  component: ToastListComponent,
+}
 
-export const ToastList = () => <ToastListComponent />
+const Template = (args) => <ToastListComponent {...args} />
+
+export const List = Template.bind({})
+List.args = {
+  toastList: [
+    {
+      title: 'Success',
+      message: 'Something was working.',
+      backgroundColor: '#5cb85c',
+      icon: '/images/toast/check.svg',
+    },
+    {
+      title: 'Info',
+      message: 'Some information',
+      backgroundColor: '#5bc0de',
+      icon: '/images/toast/info.svg',
+    },
+    {
+      title: 'Warning',
+      message: 'Be careful now!',
+      backgroundColor: '#f0ad4e',
+      icon: '/images/toast/warning.svg',
+    },
+    {
+      title: 'Error',
+      message: 'An error occured on our server.',
+      backgroundColor: '#d9534f',
+      icon: '/images/toast/error.svg',
+    },
+  ],
+}
