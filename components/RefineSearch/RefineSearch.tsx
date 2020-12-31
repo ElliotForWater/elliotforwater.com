055420c2297b454547f79d5f17d6c1b649ddbbfe
giorgia <designbygio@pm.me>
2020-12-31 12:37:30 +0000

    Merged in related-search-mobile (pull request #42)
    Related search mobile
    
    * add refined searches on mobile
    
    * fix grid children overflowing
    
    * clean up

--- components/RefineSearch/RefineSearch.tsx
+++ components/RefineSearch/RefineSearch.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from './RefineSearch.module.css'
 
 type relatedObj = {
@@ -12,14 +13,33 @@ interface Props {
 
 const RefineSearch = ({ refineSearches }: Props) => {
   return (
-    <div className={styles.tabs}>
-      {refineSearches &&
-        refineSearches.map((item, i) => (
-          <a key={i} className={styles.tab} href={`/search?query=${item.text}&type=web`}>
-            {item.text}
-          </a>
-        ))}
-    </div>
+    <>
+      <div className={styles.mobile}>
+        <article className={styles.article}>
+          <div>
+            <h6 className={styles.title}> Refine your search:</h6>
+
+            <div className={styles.tabs}>
+              {refineSearches &&
+                refineSearches.map((item, i) => (
+                  <a key={i} className={styles.tab} href={`/search?query=${item.text}&type=web`}>
+                    {item.text}
+                  </a>
+                ))}
+            </div>
+          </div>
+        </article>
+      </div>
+
+      <div className={classnames(styles.tabs, styles.desktop)}>
+        {refineSearches &&
+          refineSearches.map((item, i) => (
+            <a key={i} className={styles.tab} href={`/search?query=${item.text}&type=web`}>
+              {item.text}
+            </a>
+          ))}
+      </div>
+    </>
   )
 }
 
