a24b3d3dcf62b78d1f374871045d16e379e417a3
giorgia <designbygio@pm.me>
2021-02-19 13:47:56 +0000

    Merged in analyze (pull request #80)
    Analyze plugin + new video
    
    * add analyze plugin
    
    * add vdeo

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+const withPlugins = require('next-compose-plugins')
 const nextTranslate = require('next-translate')
-module.exports = nextTranslate()
+
+module.exports = withPlugins([[withBundleAnalyzer], nextTranslate])
