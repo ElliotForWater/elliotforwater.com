a887268e57ac8f6c93d7e2f566350be48a9b6bb7
Conrad <36226614+ConradParker@users.noreply.github.com>
2021-03-01 22:23:47 +0100

    Add allow origin to fix CORS issue

--- __mocks__/mockapiServer.js
+++ __mocks__/mockapiServer.js
@@ -5,7 +5,8 @@ const middlewares = jsonServer.defaults({ noCors: true })
 
 server.use(middlewares)
 
-server.use((res, next) => {
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Headers', '*')
   next()
 })
