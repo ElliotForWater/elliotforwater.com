2c6f6d96cfc9c559741815437eee9450b173645d
Conrad <36226614+ConradParker@users.noreply.github.com>
2021-03-03 12:37:20 +0100

    Update mockapiServer.js
    Removed the middleware as no longer used

--- __mocks__/mockapiServer.js
+++ __mocks__/mockapiServer.js
@@ -1,9 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({ noCors: true })
-
-server.use(middlewares)
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
