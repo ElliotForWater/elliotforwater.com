32aae4833704f38b41088676ebe20eb637c23fee
Conrad <36226614+ConradParker@users.noreply.github.com>
2021-03-03 12:42:00 +0100

    Update mockapiServer.js
    Remove custom headers as working with just the defaults

--- __mocks__/mockapiServer.js
+++ __mocks__/mockapiServer.js
@@ -1,12 +1,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
+server.use(middlewares)
 
 server.use('/api/searchresults', router)
 server.listen(5001, () => {
