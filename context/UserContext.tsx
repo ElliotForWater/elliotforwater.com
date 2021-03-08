cd25482eea1e3a215b6764367bf14bc8f67c967e
designbygio <gsambrotta@users.noreply.github.com>
2021-03-08 14:34:05 +0100

    fix: set initital cookies on _app.tsx (#22)
    * set init cookie on _app
    
    * add cookies value to user state

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -1,16 +1,4 @@
 import React from 'react'
-import {
-  COOKIE_NAME_LANGUAGE,
-  COOKIE_NAME_ADULT_FILTER,
-  COOKIE_NAME_NEW_TAB,
-  COOKIE_NAME_SEARCH_COUNT,
-} from '../helpers/_cookies'
-import Cookies from 'js-cookie'
-
-const searchesFromCookies = Cookies.get(COOKIE_NAME_SEARCH_COUNT)
-const languageFromCookies = Cookies.get(COOKIE_NAME_LANGUAGE)
-const filterFromCookies = Cookies.get(COOKIE_NAME_ADULT_FILTER)
-const newTabFromCookies = Cookies.get(COOKIE_NAME_NEW_TAB)
 interface userState {
   numOfSearches: number
   language: number
@@ -23,16 +11,16 @@ export interface userContextProps {
   setNextUserState: (nextState) => void
 }
 
-export const USER_STATE_DEFAULT = {
-  numOfSearches: Number(searchesFromCookies) || 0,
-  language: Number(languageFromCookies) || 1, // English
-  adultContentFilter: Number(filterFromCookies) || 1, // Moderate
-  openInNewTab: newTabFromCookies !== 'false' || false,
+export const USER_STATE_INIT = {
+  numOfSearches: 0,
+  language: 1, // English
+  adultContentFilter: 1, // Moderate
+  openInNewTab: false,
   isModalOpen: false,
 }
 
 const USER_CONTEXT_DEFAULT = {
-  userState: USER_STATE_DEFAULT,
+  userState: USER_STATE_INIT,
   setNextUserState: () => {},
 }
 
