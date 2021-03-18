a64be8d34b3a6671b94fccd5e5319de4708138fc
designbygio <gsambrotta@users.noreply.github.com>
2021-03-18 11:19:35 +0100

    fix grab adultContentFilter on search page server side, add NODE_TLS_REJECT_UNAUTHORIZED to env.development instead of package.json (#26)

--- helpers/_cookies.tsx
+++ helpers/_cookies.tsx
@@ -3,7 +3,3 @@ export const COOKIE_NAME_ADULT_FILTER = 'efw_adult_filter'
 export const COOKIE_NAME_NEW_TAB = 'efw_new_tab'
 export const COOKIE_NAME_COOKIE_CONSENT = 'efw_cookie_consent_accepted'
 export const COOKIE_NAME_SEARCH_COUNT = 'efw_search_count'
-
-export const splitCookies = function (cookies, cookieName) {
-  return cookies.split(`; ${cookieName}=`).pop().split(';').shift()
-}
