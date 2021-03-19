7806ef9c145ed63f5afd5f49d538a27bc2731d4e
gio <designbygio@protonmail.com>
2021-03-19 10:53:42 +0100

    remove SearchFilter

--- components/SearchFilters/SearchFilters.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import SearchFiltersComp from './SearchFilters'
-
-export default { title: 'Components/Search Filters' }
-
-export const SearchFilters = () => <SearchFiltersComp />
