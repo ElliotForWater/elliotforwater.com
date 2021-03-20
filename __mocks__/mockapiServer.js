f365df0eade9424b42bde34dec156b5b2db5cee2
Conrad <36226614+ConradParker@users.noreply.github.com>
2021-03-20 08:44:07 +0100

    Fix mock api cors errors (#20)
    * Add allow any headers on local host
    
    * Add allow origin to fix CORS issue
    
    * Update mockapiServer.js
    
    Removed the middleware as no longer used
    
    * Update mockapiServer.js
    
    Remove custom headers as working with just the defaults

--- __mocks__/mockapiServer.js
+++ __mocks__/mockapiServer.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({ noCors: true })
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
