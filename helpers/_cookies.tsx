eb9133e854788f458c69ecc6b394e93ab33e3700
xMartin <xmartin@siarp.de>
2021-03-28 15:20:02 +0200

    refactor: move cookie-related code to cookies helper

--- helpers/_cookies.tsx
+++ helpers/_cookies.tsx
@@ -1,5 +1,24 @@
+import Cookies from 'js-cookie'
+
 export const COOKIE_NAME_LANGUAGE = 'efw_language'
 export const COOKIE_NAME_ADULT_FILTER = 'efw_adult_filter'
 export const COOKIE_NAME_NEW_TAB = 'efw_new_tab'
 export const COOKIE_NAME_COOKIE_CONSENT = 'efw_cookie_consent_accepted'
 export const COOKIE_NAME_SEARCH_COUNT = 'efw_search_count'
+
+type CookieName =
+  | typeof COOKIE_NAME_LANGUAGE
+  | typeof COOKIE_NAME_ADULT_FILTER
+  | typeof COOKIE_NAME_NEW_TAB
+  | typeof COOKIE_NAME_COOKIE_CONSENT
+  | typeof COOKIE_NAME_SEARCH_COUNT
+
+export type CookieMap = { [cookieName: string]: string }
+
+export function getCookie(name: CookieName, cookieMap?: CookieMap): string {
+  return cookieMap ? cookieMap[name] : Cookies.get(name)
+}
+
+export function setCookie(name: CookieName, value: string, opts?: { expires: number }): void {
+  Cookies.set(name, value, opts)
+}
--- hooks/useUserStateSyncedWithCookies.tsx
+++ hooks/useUserStateSyncedWithCookies.tsx
@@ -5,46 +5,42 @@ import {
   COOKIE_NAME_ADULT_FILTER,
   COOKIE_NAME_NEW_TAB,
   COOKIE_NAME_SEARCH_COUNT,
+  getCookie,
+  CookieMap,
+  setCookie,
 } from '../helpers/_cookies'
-import Cookies from 'js-cookie'
 
-type CookieMap = { [cookieName: string]: string }
+const mergeCookiesWithUserState = (defaultUserState: UserState, serverCookies?: CookieMap): UserState => {
+  const newUserState = { ...defaultUserState }
+
+  const numOfSearches = getCookie(COOKIE_NAME_SEARCH_COUNT, serverCookies)
+  if (numOfSearches !== undefined) {
+    newUserState.numOfSearches = Number(numOfSearches)
+  }
+
+  const language = getCookie(COOKIE_NAME_LANGUAGE, serverCookies)
+  if (language !== undefined) {
+    newUserState.language = Number(language)
+  }
+
+  const adultContentFilter = getCookie(COOKIE_NAME_ADULT_FILTER, serverCookies)
+  if (adultContentFilter !== undefined) {
+    newUserState.adultContentFilter = Number(adultContentFilter)
+  }
+
+  const openInNewTab = getCookie(COOKIE_NAME_NEW_TAB, serverCookies)
+  if (openInNewTab !== undefined) {
+    newUserState.openInNewTab = openInNewTab !== 'false'
+  }
+
+  return newUserState
+}
 
 const cookiesName = {
   numOfSearches: COOKIE_NAME_SEARCH_COUNT,
   language: COOKIE_NAME_LANGUAGE,
   adultContentFilter: COOKIE_NAME_ADULT_FILTER,
   openInNewTab: COOKIE_NAME_NEW_TAB,
-}
-
-const mergeCookiesWithUserState = (defaultUserState: UserState, serverCookies?: CookieMap): UserState => {
-  const newUserState = { ...defaultUserState }
-
-  function getCookie(name: string): string {
-    return serverCookies ? serverCookies[name] : Cookies.get(name)
-  }
-
-  const numOfSearches = getCookie(cookiesName.numOfSearches)
-  if (numOfSearches !== undefined) {
-    newUserState.numOfSearches = Number(numOfSearches)
-  }
-
-  const language = getCookie(cookiesName.language)
-  if (language !== undefined) {
-    newUserState.language = Number(language)
-  }
-
-  const adultContentFilter = getCookie(cookiesName.adultContentFilter)
-  if (adultContentFilter !== undefined) {
-    newUserState.adultContentFilter = Number(adultContentFilter)
-  }
-
-  const openInNewTab = getCookie(cookiesName.openInNewTab)
-  if (openInNewTab !== undefined) {
-    newUserState.openInNewTab = openInNewTab !== 'false'
-  }
-
-  return newUserState
 }
 
 export const useUserStateSyncedWithCookies = (serverCookies?: CookieMap): UserContextProps => {
@@ -60,7 +56,7 @@ export const useUserStateSyncedWithCookies = (serverCookies?: CookieMap): UserCo
 
       for (const key in nextState) {
         if (Object.getOwnPropertyDescriptor(cookiesName, key)) {
-          Cookies.set(cookiesName[key], newState[key], { expires: 365 })
+          setCookie(cookiesName[key], newState[key], { expires: 365 })
         }
       }
 
