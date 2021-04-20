26c232cb859cd6e0ca52068b5b0ed886c7412d9e
designbygio <designbygio@pm.me>
2021-04-20 13:29:31 +0200

    fix build

--- components/Hero/Hero.stories.tsx
+++ components/Hero/Hero.stories.tsx
@@ -8,7 +8,6 @@ export default {
 export const HeroWithAddtoBrowserBtn = () => (
   <HeroComponent
     imageUrl='https://images.ctfassets.net/3qtdrevau8ui/5hMuK9cWqpVntZ21DhF4YE/625c296a5ca47192171006e60c9aca4e/piggy-money.png'
-    imageTitle='backgrodunimage'
     title="I'm a super hero!!"
     subtitle='I will save you forever and ever and ever...'
     withBrowserCta
@@ -18,7 +17,6 @@ export const HeroWithAddtoBrowserBtn = () => (
 export const HeroWithCta = () => (
   <HeroComponent
     imageUrl='https://images.ctfassets.net/3qtdrevau8ui/5hMuK9cWqpVntZ21DhF4YE/625c296a5ca47192171006e60c9aca4e/piggy-money.png'
-    imageTitle='backgrodunimage'
     title="I'm a super hero!!"
     subtitle='I will save you forever and ever and ever...'
     ctaLabel='please click me'
@@ -29,7 +27,6 @@ export const HeroWithCta = () => (
 export const HeroJustTitle = () => (
   <HeroComponent
     imageUrl='https://images.ctfassets.net/3qtdrevau8ui/5hMuK9cWqpVntZ21DhF4YE/625c296a5ca47192171006e60c9aca4e/piggy-money.png'
-    imageTitle='backgrodunimage'
     title="I'm a super hero!!"
   />
 )
--- components/Hero/Hero.test.tsx
+++ components/Hero/Hero.test.tsx
@@ -7,7 +7,6 @@ describe('Hero', () => {
     const wrap = shallow(
       <Hero
         imageUrl='https://images.ctfassets.net/3qtdrevau8ui/5hMuK9cWqpVntZ21DhF4YE/625c296a5ca47192171006e60c9aca4e/piggy-money.png'
-        imageTitle='backgrodunimage'
         title="I'm a super hero!!"
         subtitle='I will save you forever and ever and ever...'
         withBrowserCta
