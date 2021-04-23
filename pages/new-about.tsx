23dd687f1fb83b8943bb816c67153a9b08b7641f
designbygio <designbygio@pm.me>
2021-04-23 14:17:19 +0200

    change to getinitialprops, add loading, hero subtitle

--- pages/new-about.tsx
+++ pages/new-about.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import useTranslation from 'next-translate/useTranslation'
 import Layout from '../components/Layout/Layout'
 import Hero from '../components/Hero/Hero'
@@ -6,15 +6,17 @@ import fetchContenful from '../helpers/_fetchContentful'
 import ContactUsForm from '../components/Forms/Contact/ContactForm'
 import dynamic from 'next/dynamic'
 import Person from '../components/Person/Person'
+import Loader from '../components/Loader/Loader'
 
 const Projectslides = dynamic(() => import('../components/Sliders/ProjectSliders'), { ssr: false })
 
-export async function getStaticProps() {
+About.getInitialProps = async () => {
   const { aboutUsPage } = await fetchContenful(`
   {
     aboutUsPage(id: "74N0U1LNjfceeBVytfHC93", preview: false){
       hero{
         title,
+        subtitle,
         backgroundImage{
           url
         }
@@ -88,14 +90,20 @@ export async function getStaticProps() {
   }`)
 
   return {
-    props: {
-      aboutUsPage: aboutUsPage,
-    },
+    aboutUsPage: aboutUsPage,
   }
 }
 
-function About(aboutUsPage) {
+function About({ aboutUsPage }) {
   const { t } = useTranslation()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (aboutUsPage) {
+      setIsLoading(false)
+    }
+  }, [])
+
   const {
     hero,
     firstSectionTitle,
@@ -113,95 +121,101 @@ function About(aboutUsPage) {
     opensourceTitle,
     opensourceDescription,
     contactUsTitle,
-  } = aboutUsPage.aboutUsPage
+  } = aboutUsPage
 
   return (
     <Layout pageTitle={t('about:pageTitle')} pageDescription={t('about:pageTitle')} fluid>
-      <Hero imageUrl={hero.backgroundImage.url} title={hero.title} withBrowserCta />
-      <section className='sections'>
-        <div className='containerCenter'>
-          <h2 className='titleWithDivider'>{firstSectionTitle}</h2>
-          <div className='divider' />
-          <p>{firstSectionContent}</p>
-        </div>
-      </section>
-      <section className='sections'>
-        <div className='containerCenter'>
-          <blockquote>{quote}</blockquote>
-        </div>
-      </section>
-      <section>
-        <div className='currentProjectWrap'>
-          <Projectslides slides={projectsCarouselCollection.items} />
-        </div>
-      </section>
-      <section className='sections'>
-        <div className='containerCenter'>
-          <h2>{history}</h2>
-          <div className='historyDatesContainer'>
-            {historyDatesCollection.items.map(({ date, title, description }, index) => (
-              <div className='historyWrap' key={index}>
-                <h4>{date}</h4>
-                <div>{title}</div>
-                <div className='historyDivider' />
-                <p>{description}</p>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Hero imageUrl={hero.backgroundImage.url} title={hero.title} subtitle={hero.subtitle} withBrowserCta />
+          <section className='sections'>
+            <div className='containerCenter'>
+              <h2 className='titleWithDivider'>{firstSectionTitle}</h2>
+              <div className='divider' />
+              <p>{firstSectionContent}</p>
+            </div>
+          </section>
+          <section className='sections'>
+            <div className='containerCenter'>
+              <blockquote>{quote}</blockquote>
+            </div>
+          </section>
+          <section>
+            <div className='currentProjectWrap'>
+              <Projectslides slides={projectsCarouselCollection.items} />
+            </div>
+          </section>
+          <section className='sections'>
+            <div className='containerCenter'>
+              <h2>{history}</h2>
+              <div className='historyDatesContainer'>
+                {historyDatesCollection.items.map(({ date, title, description }, index) => (
+                  <div className='historyWrap' key={index}>
+                    <h4>{date}</h4>
+                    <div>{title}</div>
+                    <div className='historyDivider' />
+                    <p>{description}</p>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      <section className='sections'>
-        <div className='containerCenter'>
-          <h2>{founderTitle}</h2>
-          <div className='founderWrap'>
-            <Person profilePic={founder.profilePic} longDescription={founder.longDescription} size='big' />
-          </div>
-        </div>
-      </section>
-      <section className='sections'>
-        <div className='teamContainer'>
-          <h2 className='titleWithDivider'>{team}</h2>
-          <div className='divider' />
-          <div className='teamWrap'>
-            {teamPicCollection.items.map((member, index) => (
-              <Person
-                key={index}
-                name={member.name}
-                profilePic={member.profilePic}
-                shortDescription={member.shortDescription}
-              />
-            ))}
-          </div>
-        </div>
-        <div className='volunteersContainer'>
-          <h4 className='volunteersTitle'>{volunteerTitle}</h4>
-          <div className='volunteersWrap'>
-            {volunteersPicCollection.items.map((member, index) => (
-              <Person
-                key={index}
-                name={member.name}
-                profilePic={member.profilePic}
-                shortDescription={member.shortDescription}
-                size='small'
-              />
-            ))}
-          </div>
-        </div>
-      </section>
-      <section className='sections'>
-        <div className='containerCenter opensourceContainer'>
-          <h2 className='titleWithDivider'>{opensourceTitle}</h2>
-          <div className='divider' />
-          <p>{opensourceDescription}</p>
-          <a href='https://github.com/ElliotForWater/efw-webapp'>https://github.com/ElliotForWater/efw-webapp</a>
-        </div>
-      </section>
-      <section className='sections'>
-        <div className='containerCenter'>
-          <h2>{contactUsTitle}</h2>
-          <ContactUsForm />
-        </div>
-      </section>
+            </div>
+          </section>
+          <section className='sections'>
+            <div className='containerCenter'>
+              <h2>{founderTitle}</h2>
+              <div className='founderWrap'>
+                <Person profilePic={founder.profilePic} longDescription={founder.longDescription} size='big' />
+              </div>
+            </div>
+          </section>
+          <section className='sections'>
+            <div className='teamContainer'>
+              <h2 className='titleWithDivider'>{team}</h2>
+              <div className='divider' />
+              <div className='teamWrap'>
+                {teamPicCollection.items.map((member, index) => (
+                  <Person
+                    key={index}
+                    name={member.name}
+                    profilePic={member.profilePic}
+                    shortDescription={member.shortDescription}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className='volunteersContainer'>
+              <h4 className='volunteersTitle'>{volunteerTitle}</h4>
+              <div className='volunteersWrap'>
+                {volunteersPicCollection.items.map((member, index) => (
+                  <Person
+                    key={index}
+                    name={member.name}
+                    profilePic={member.profilePic}
+                    shortDescription={member.shortDescription}
+                    size='small'
+                  />
+                ))}
+              </div>
+            </div>
+          </section>
+          <section className='sections'>
+            <div className='containerCenter opensourceContainer'>
+              <h2 className='titleWithDivider'>{opensourceTitle}</h2>
+              <div className='divider' />
+              <p>{opensourceDescription}</p>
+              <a href='https://github.com/ElliotForWater/efw-webapp'>https://github.com/ElliotForWater/efw-webapp</a>
+            </div>
+          </section>
+          <section className='sections'>
+            <div className='containerCenter'>
+              <h2>{contactUsTitle}</h2>
+              <ContactUsForm />
+            </div>
+          </section>
+        </>
+      )}
       <style jsx>
         {`
           .sections {
