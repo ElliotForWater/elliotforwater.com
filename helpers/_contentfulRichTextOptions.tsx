99e45dd3a254f2d60df4ae1e189479ab554488c7
designbygio <designbygio@pm.me>
2021-11-27 13:10:35 +0100

    add rich text options

--- helpers/_contentfulRichTextOptions.tsx
+++ helpers/_contentfulRichTextOptions.tsx
@@ -1,73 +1,115 @@
-import { BLOCKS, INLINES } from '@contentful/rich-text-types'
+// https://github.com/whitep4nth3r/p4nth3rblog/blob/main/components/RichTextPageContent/index.js
 
-export default function renderOptions(links) {
-  // create an asset map
-  const assetMap = new Map()
-  // loop through the assets and add them to the map
-  for (const asset of links.assets.block) {
-    assetMap.set(asset.sys.id, asset)
-  }
+import BlogPostEmbed from '../components/RichTextOptions/BlogPostEmbed'
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types'
 
-  // create an entry map
+export function getRichTextRenderOptions(links) {
+  const assetBlockMap = new Map(links?.assets?.block?.map((asset) => [asset.sys.id, asset]))
   const entryMap = new Map()
-  // loop through the block linked entries and add them to the map
-  for (const entry of links.entries.block) {
-    entryMap.set(entry.sys.id, entry)
+  if (links?.assets.block) {
+    for (const entry of links.assets.block) {
+      entryMap.set(entry.sys.id, entry)
+    }
   }
 
-  // loop through the inline linked entries and add them to the map
-  for (const entry of links.entries.inline) {
-    entryMap.set(entry.sys.id, entry)
+  if (links?.assets.inline) {
+    for (const entry of links.assets.inline) {
+      entryMap.set(entry.sys.id, entry)
+    }
   }
 
   return {
-    // other options...
+    renderMark: {
+      [MARKS.BOLD]: (text) => <b>{text}</b>,
+      [MARKS.CODE]: (text) => <code>{text}</code>,
+    },
 
     renderNode: {
-      // other options...
-      [INLINES.EMBEDDED_ENTRY]: (node, children) => {
-        // find the entry in the entryMap by ID
-        const entry = entryMap.get(node.data.target.sys.id)
+      [INLINES.HYPERLINK]: (node, children) => (
+        <a href={node.data.uri} target='_blank' rel='nofollow noreferrer'>
+          {children}
+        </a>
+      ),
+      // [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+      //   const entry = entryMap.get(node.data.target.sys.id);
+      //   const { __typename } = entry;
 
-        // render the entries as needed
-        if (entry.__typename === 'BlogPost') {
-          return <a href={`/blog/${entry.slug}`}>{entry.title}</a>
-        }
-      },
+      //   switch (__typename) {
+      //     case "BlogPost":
+      //       const { slug, title, featuredImage, excerpt } = entry;
+
+      //       return (
+      //         <>
+      //           <ReactTooltip wrapper="span" />
+      //           <Link href={`/blog/${slug}`}>
+      //             <a
+      //               data-arrow-color="#ffb626"
+      //               data-effect="solid"
+      //               data-html={true}
+      //               data-type="dark"
+      //               data-tip={buildBlogPostLinkTooltipHTML(
+      //                 title,
+      //                 featuredImage,
+      //                 excerpt,
+      //               )}
+      //             >
+      //               {title}
+      //             </a>
+      //           </Link>
+      //         </>
+      //       );
+      //     default:
+      //       return null;
+      //   }
+      // },
+      [BLOCKS.HR]: (text) => <hr />,
+      [BLOCKS.QUOTE]: (node, children) => <blockquote>{children}</blockquote>,
+      [BLOCKS.UL_LIST]: (node, children) => <ul>{children}</ul>,
+      [BLOCKS.OL_LIST]: (node, children) => <ol>{children}</ol>,
+      [BLOCKS.LIST_ITEM]: (node, children) => <li>{children}</li>,
       [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
-        // find the entry in the entryMap by ID
         const entry = entryMap.get(node.data.target.sys.id)
+        const { __typename } = entry
 
-        // render the entries as needed by looking at the __typename
-        // referenced in the GraphQL query
-        if (entry.__typename === 'CodeBlock') {
-          return (
-            <pre>
-              <code>{entry.code}</code>
-            </pre>
-          )
-        }
+        switch (__typename) {
+          case 'BlogPost':
+            return <BlogPostEmbed item={entry} />
+          // case "TweetEmbed":
+          //   const { tweetId } = entry;
 
-        if (entry.__typename === 'VideoEmbed') {
-          return (
-            <iframe
-              src={entry.embedUrl}
-              height='100%'
-              width='100%'
-              frameBorder='0'
-              scrolling='no'
-              title={entry.title}
-              allowFullScreen
-            />
-          )
+          //   return <DynamicTweetEmbed tweetId={tweetId} />;
+          case 'VideoEmbed':
+            /* eslint-disable-next-line no-case-declarations */
+            const { embedUrl, title } = entry
+
+            return (
+              <div>
+                <iframe
+                  src={embedUrl}
+                  height='100%'
+                  width='100%'
+                  frameBorder='0'
+                  scrolling='no'
+                  title={title}
+                  allowFullScreen
+                />
+              </div>
+            )
+          // case "CodeBlock":
+          //   const { language, code } = entry;
+          //   return <DynamicCodeBlock language={language} code={code} />;
+          default:
+            return null
         }
       },
       [BLOCKS.EMBEDDED_ASSET]: (node, next) => {
-        // find the asset in the assetMap by ID
-        const asset = assetMap.get(node.data.target.sys.id)
+        const image: any = assetBlockMap.get(node.data.target.sys.id)
 
-        // render the asset accordingly
-        return <img src={asset.url} alt='My image alt text' />
+        return (
+          <div>
+            <img src={image.url} alt={image.title} />
+          </div>
+        )
       },
     },
   }
