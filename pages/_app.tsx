6852e4c7e1d0acc17278224d27f9224379b0fad7
designbygio <gsambrotta@users.noreply.github.com>
2021-12-01 16:57:06 +0100

    Terms privacy update (#145)
    * modify term page and style
    
    * update privacy page and clean console log app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,10 +15,8 @@ import { CookieMap } from '../helpers/_cookies'
 // Binding routes events.
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', (url) => {
-  console.log('router changed')
   console.log('envroment', process.env.NEXT_PUBLIC_IS_PRODUCTION)
   if (process.env.NEXT_PUBLIC_IS_PRODUCTION) {
-    console.log('is production router changed')
     gtag.pageview(document.title, location.href, url)
   }
   NProgress.done()
@@ -33,10 +31,7 @@ function ElliotApp({ Component, pageProps, serverCookies }: ElliotAppProps) {
   const user = useUserStateSyncedWithCookies(serverCookies)
   useEffect(() => {
     import('../webComponents/CookiePolicy/CookiePolicy')
-    console.log('app reload')
-    console.log('envroment useEffect', process.env.NEXT_PUBLIC_IS_PRODUCTION)
     if (process.env.NEXT_PUBLIC_IS_PRODUCTION) {
-      console.log('is production useEffect')
       gtag.pageview(document.title, location.href, location.pathname + location.search)
     }
   }, [])
--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -10,65 +10,63 @@ function Privacy() {
 
   return (
     <Layout fluid pageTitle={t('privacy:pageTitle')} pageDescription={t('privacy:pageDescription')}>
-      <div className='old_container'>
-        <h2 className='title'>{t('privacy:title')}</h2>
+      <div className='internal-page-container'>
+        <h2>{t('privacy:title')}</h2>
         <p>{t('privacy:description')}</p>
-      </div>
-      <div className='content full' id='content'>
-        <div className='old_container'>
-          <h3>{t('privacy:subtitle1')}</h3>
-          <Trans
-            i18nKey='privacy:p1'
-            /* eslint-disable-next-line react/jsx-key */
-            components={[<ComponentDescription />, <br />]}
-          />
-          <h3>{t('privacy:subtitle2')}</h3>
-          <Trans
-            i18nKey='privacy:p2'
-            /* eslint-disable-next-line react/jsx-key */
-            components={[<ComponentDescription />, <br />]}
-          />
-          <h3>{t('privacy:subtitle3')}</h3>
-          <Trans
-            i18nKey='privacy:p3'
-            /* eslint-disable-next-line react/jsx-key */
-            components={[<ComponentDescription />, <br />]}
-          />
-          <h3>{t('privacy:subtitle4')}</h3>
-          <Trans
-            i18nKey='privacy:p4_a'
-            /* eslint-disable react/jsx-key */
-            components={[
-              <ComponentDescription />,
-              <a href='http://www.codefuel.com/legal/end_user_privacy_policy' target='_blank' />,
-              <a href='http://go.microsoft.com/fwlink/?LinkId=521839' target='_blank' />,
-            ]}
-            /* eslint-enable react/jsx-key */
-          />
-          <p>{t('privacy:p4_b')}</p>
-          <p>{t('privacy:p4_c')}</p>
-          <p>{t('privacy:p4_d')} </p>
+        <div className='internal-page-divider' />
 
-          <h3>{t('privacy:subtitle5')}</h3>
-          <p>{t('privacy:p5')}</p>
+        <h4>{t('privacy:subtitle1')}</h4>
+        <Trans
+          i18nKey='privacy:p1'
+          /* eslint-disable-next-line react/jsx-key */
+          components={[<ComponentDescription />, <br />]}
+        />
+        <h4>{t('privacy:subtitle2')}</h4>
+        <Trans
+          i18nKey='privacy:p2'
+          /* eslint-disable-next-line react/jsx-key */
+          components={[<ComponentDescription />, <br />]}
+        />
+        <h4>{t('privacy:subtitle3')}</h4>
+        <Trans
+          i18nKey='privacy:p3'
+          /* eslint-disable-next-line react/jsx-key */
+          components={[<ComponentDescription />, <br />]}
+        />
+        <h4>{t('privacy:subtitle4')}</h4>
+        <Trans
+          i18nKey='privacy:p4_a'
+          /* eslint-disable react/jsx-key */
+          components={[
+            <ComponentDescription />,
+            <a href='http://www.codefuel.com/legal/end_user_privacy_policy' target='_blank' />,
+            <a href='http://go.microsoft.com/fwlink/?LinkId=521839' target='_blank' />,
+          ]}
+          /* eslint-enable react/jsx-key */
+        />
+        <p>{t('privacy:p4_b')}</p>
+        <p>{t('privacy:p4_c')}</p>
 
-          <h3>{t('privacy:subtitle6')}</h3>
-          <p>{t('privacy:p6')}</p>
+        <h4>{t('privacy:subtitle5')}</h4>
+        <p>{t('privacy:p5')}</p>
 
-          <h3>{t('privacy:subtitle7')}</h3>
-          <Trans
-            i18nKey='privacy:p7'
-            /* eslint-disable-next-line react/jsx-key */
-            components={[<ComponentDescription />, <br />]}
-          />
+        <h4>{t('privacy:subtitle6')}</h4>
+        <p>{t('privacy:p6')}</p>
 
-          <h3>Y{t('privacy:subtitle8')}</h3>
-          <p>{t('privacy:p8')}</p>
+        <h4>{t('privacy:subtitle7')}</h4>
+        <Trans
+          i18nKey='privacy:p7'
+          /* eslint-disable-next-line react/jsx-key */
+          components={[<ComponentDescription />, <br />]}
+        />
 
-          <p>Email: info@elliotforwater.com</p>
-          <br />
-        </div>{' '}
-        {/* <!-- end container --> */}
+        <h4>{t('privacy:subtitle8')}</h4>
+        <p>{t('privacy:p8')}</p>
+
+        <p>
+          Email: <a href='mailto:info@elliotforwater.com'>info@elliotforwater.com</a>
+        </p>
+        <br />
       </div>
     </Layout>
   )
