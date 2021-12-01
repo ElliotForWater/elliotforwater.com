a84fb150b43236fcdd9f5b541fa196ca3bb5772a
designbygio <designbygio@pm.me>
2021-12-01 16:06:43 +0100

    modify term page and style

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -7,122 +7,47 @@ function Terms() {
 
   return (
     <Layout fluid pageTitle={t('terms:pageTitle')} pageDescription={t('terms:pageDescription')}>
-      <div className='old_container'>
+      <div className='internal-page-container'>
         <h2>{t('terms:title')}</h2>
-        <label>{t('terms:date')}</label>
-        <div className='termsWrap'>
-          <br />
-          <br />
-          {t('terms:p1')}
-          <br />
-          <br />
-          {t('terms:p2')}
-          <br />
-          <br />
-          {t('terms:p3')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle1')}</strong>
-          <br />
-          <br />
-          {t('terms:p4')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle2')}</strong>
-          <br />
-          <br />
-          {t('terms:p5')}
-          <br />
-          <br />
-          {t('terms:p6')}
-          <br />
-          <br />
-          {t('terms:p7')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle3')}</strong>
-          <br />
-          <br />
-          {t('terms:p8')}
-          <br />
-          <br />
-          {t('terms:p9')}
-          <br />
-          <br />
-          {t('terms:p10')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle4')}</strong>
-          <br />
-          <br />
-          {t('terms:p11')}
-          You agree to defend, indemnify and hold harmless Elliot For Water LTD and its licensee and licensors, and
-          their employees, contractors, agents, officers and directors, from and against any and all claims, damages,
-          obligations, losses, liabilities, costs or debt, and expenses (including but not limited to attorney's fees),
-          resulting from or arising out of a) your use and access of the Service, or b) a breach of these Terms.
-          <br />
-          <br />
-          <strong>{t('terms:subtitle5')}</strong>
-          <br />
-          <br />
-          {t('terms:p12')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle6')}</strong>
-          <br />
-          <br />
-          {t('terms:p13')}
-          <br />
-          <br />
-          {t('terms:p14')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle7')}</strong>
-          <br />
-          <br />
-          {t('terms:p15')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle8')}</strong>
-          <br />
-          <br />
-          {t('terms:p16')}
-          <br />
-          <br />
-          {t('terms:p17')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle9')}</strong>
-          <br />
-          <br />
-          {t('terms:p18')}
-          <br />
-          <br />
-          {t('terms:p19')}
-          <br />
-          <br />
-          <strong>{t('terms:subtitle10')}</strong>
-          <br />
-          <br />
-          {t('terms:p20')}
-          <br />
-          <br />
-          {t('terms:p21')} info@elliotforwater.com
-          <br />
-          <br />
-          {t('terms:p22')}
-          <br />
-          <br />
+        <div>{t('terms:date')}</div>
+        <div className='internal-page-divider' />
+
+        <div className='internal-page-text'>
+          <p>{t('terms:p1')}</p>
+          <p>{t('terms:p2')}</p>
+          <p>{t('terms:p3')}</p>
+          <h4>{t('terms:subtitle1')}</h4>
+          <p>{t('terms:p4')}</p>
+          <h4>{t('terms:subtitle2')}</h4>
+          <p>{t('terms:p5')}</p>
+          <p>{t('terms:p6')}</p>
+          <p>{t('terms:p7')}</p>
+          <h4>{t('terms:subtitle3')}</h4>
+          <p>{t('terms:p8')}</p>
+          <p>{t('terms:p9')}</p>
+          <p>{t('terms:p10')}</p>
+          <h4>{t('terms:subtitle4')}</h4>
+          <p>{t('terms:p11')}</p>
+          <h4>{t('terms:subtitle5')}</h4>
+          <p>{t('terms:p12')}</p>
+          <h4>{t('terms:subtitle6')}</h4>
+          <p>{t('terms:p13')}</p>
+          <p>{t('terms:p14')}</p>
+          <h4>{t('terms:subtitle7')}</h4>
+          <p>{t('terms:p15')}</p>
+          <h4>{t('terms:subtitle8')}</h4>
+          <p>{t('terms:p16')}</p>
+          <p>{t('terms:p17')}</p>
+          <h4>{t('terms:subtitle9')}</h4>
+          <p>{t('terms:p18')}</p>
+          <p>{t('terms:p19')}</p>
+          <h4>{t('terms:subtitle10')}</h4>
+          <p>{t('terms:p20')}</p>
+          <p>
+            {t('terms:p21')} <a href='mailto:info@elliotforwater.com'>info@elliotforwater.com</a>
+          </p>
         </div>
-      </div>{' '}
-      {/* <!-- end old_container --> */}
-      <style jsx>
-        {`
-          .termsWrap {
-            max-width: 700px;
-          }
-        `}
-      </style>
+      </div>
     </Layout>
   )
 }
